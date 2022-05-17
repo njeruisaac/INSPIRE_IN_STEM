a2190bd7fd3e14cf874add42cea9192b2ee38567
Mzito <tumba.app@gmail.com>
2022-05-17 17:34:51 +0300

    Practice with Strings and Integers

--- lesson1.py
+++ lesson1.py
@@ -4,9 +4,15 @@ length = int(input("What is the Length of the Cube"))
 volume = length * length * length
 print("Volume of the cube is equals to " + str(volume) + "cm3")
 
-#Surface area of a cylinder
+#Surface area of a Cylinder
 radius = int(input("What is the Radius of the cylinder"))
 height = int(input("What is the Height of the cylinder"))
 PI= 3.142
 surfaceArea= 2 * PI * radius * height + 2 * PI * radius * radius 
 print("The suface area of the cylinder is " + str(surfaceArea) + "cm2")
+
+#Volume of a Cylinder
+volume_cylinder = PI * radius * radius * height
+radius = int(input("Radius of the Cylinder"))
+height = int(input("Height of the cylinder"))
+print("The volume of the cylinder is " + str(volume_cylinder) + "cm3")
---
+++ lesson2.py
@@ -0,0 +1,15 @@
+from stat import FILE_ATTRIBUTE_OFFLINE
+
+
+first_name = "Isaac"
+second_name = "Njeru"
+school_attended = "JKUAT"
+favourite_food = "Pilau"
+birthday_year = "2002"
+birthday_month ="March"
+print("My name is " + first_name + second_name)
+print("I was born in " + birthday_year + birthday_month)
+print("My favourite food is " + favourite_food)
+print("I am currently at" + school_attended)
+age = int(2022) - int(birthday_year) 
+print("I am " + str(age) + "Years Old")
