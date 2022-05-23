c0e4f9e42607de1b0f8257be4d78efdc7a71423a
Mzito <tumba.app@gmail.com>
2022-05-23 10:32:49 +0300

    Initialising Directories and For loop function

---
+++ lesson7.py
@@ -0,0 +1,28 @@
+#1/usr/bin/python
+
+##
+#   Dictionaries
+#   Name: Isaac Njeru
+#   Date: 23/5/2022
+
+# student = {"Name":"Isaac","Age": "20","Gender":"Male"}
+# student["Id Number"]= "39277231"
+# student["Hobbies"]="Music Production"
+# student["Favourite Color"]="Black"
+# student["Favourite Meal"]="Chapati"
+# print(student["Name"])
+# print(student["Age"])
+# print(student["Gender"])
+# print(student["Hobbies"])
+# print(student["Favourite Color"])
+# print(student["Favourite Meal"])
+
+#Initialise an empty dictionary
+# variable={}(calibraces)
+student={}
+student["homeCity"]="Nairobi"
+student["preferredSport"]="Rugby"
+student["instrumentPlayed"]="Piano"
+student["Name"]="Jeff"
+del student["Name"]
+print(student)
--- loop.py
+++ loop.py
@@ -1,8 +1,14 @@
-school = ['Peace','Love','Hope','Joy' ]
-pupil = [' Mark',' Luke ',' Matthew ',' David',' James',]
+#1/usr/bin/python
+#   For Loop
+# school = ['Peace','Love','Hope','Joy' ]
+# pupil = [' Mark',' Luke ',' Matthew ',' David',' James',]
 # print(school[0]+ pupil[0],school[0]+ pupil[1],school[2]+ pupil[2],school[3]+ pupil[3],school[3]+ pupil[4], )
 # print(f"I am {pupil[1]} and I school at {school[1]}")
 
 #Using Loop Statement
-for pupil in pupil:
-    print(f"Hello I am pupil {pupil} ")
+# for pupil in pupil:
+#     print(f"Hello I am pupil {pupil} ")
+for number in range(0,9):
+    print(number**2)
+    
+
