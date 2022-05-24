9d7bd55d523b52d6ba77e9c16722255219e80e66
Mzito <tumba.app@gmail.com>
2022-05-24 09:25:33 +0300

    Assignment 2 23rd may

--- lesson8.py
+++ lesson8.py
@@ -1,21 +1,28 @@
 #1/usr/bin/python
 #Square
-squares =[]
-for number in range (0,10):
-    square= number**2
-    squares.append(square)
-print(squares)
+# squares =[]
+# for number in range (0,10):
+#     square= number**2
+#     squares.append(square)
+# print(squares)
 
-#Cube
-cubes=[]
-for number in range(0,10):
-    cube= number**3
-    cubes.append(cube)
-    print(cubes)
+# #Cube
+# cubes=[]
+# for number in range(0,10):
+#     cube= number**3
+#     cubes.append(cube)
+# print(cubes)
 
-#Sum
-sums=[]
-for number in range(0,10):
-    sum=number+number
-    sums.append(sum)
-    print(sums)
+# #Sum
+# sums=[]
+# for number in range(0,10):
+#     sum=number+number
+#     sums.append(sum)
+# print(sums)
+
+#IF STATEMENTS - conditions
+age = int(input("What is your age?"))
+if age>= 18:
+    print("You are able to vote")
+else:
+    print("Too Young to vote")
